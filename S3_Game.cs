b5f72f55c68a30d69d7ed278547375a7994d4dda
DPRLive <chosy0107@naver.com>
2020-07-11 23:42:06 +0900

    textRPG2 complete

--- S3_Game.cs
+++ S3_Game.cs
@@ -13,6 +13,8 @@ namespace Csharp_Study
     {
         private GameMode mode = GameMode.Lobby;
         private S3_Player player = null;
+        private S3_Monster monster = null;
+        protected Random rand = new Random();
 
         public void process()
         {
@@ -25,7 +27,7 @@ namespace Csharp_Study
                     processTown();
                     break;
                 case GameMode.Field :
-                    //processField();
+                    processField();
                     break;
             }
         }
@@ -72,5 +74,80 @@ namespace Csharp_Study
                     break;
             }
         }
+
+        private void processField()
+        {
+            Console.WriteLine("필드에 입장했습니다!");
+            Console.WriteLine("[1] 몬스터와 전투하기");
+            Console.WriteLine("[2] 일정 확률로 마을 돌아가기");
+
+            createRandomMonster();
+
+            string input = Console.ReadLine();
+            switch (input)
+            {
+                case "1" :
+                    processFight();
+                    break;
+                case "2" :
+                    tryEscape();
+                    break;
+            }
+        }
+
+        private void createRandomMonster()
+        {
+            int randValue = rand.Next(0,3);
+            switch (randValue)
+            {
+                case 0 :
+                    monster = new Slime();
+                    Console.WriteLine("슬라임이 생성되었습니다.");
+                    break;
+                case 1 :
+                    monster = new Orc();
+                    Console.WriteLine("오크가 생성되었습니다.");
+                    break;
+                case 2 :
+                    monster = new Skeleton();
+                    Console.WriteLine("스켈레톤이 생성되었습니다.");
+                    break;
+            }
+        }
+
+        private void processFight()
+        {
+            while(true)
+            {
+                int damage = player.getAttack();
+                monster.onDamaged(damage);
+                if (monster.isDead())
+                {
+                    Console.WriteLine("승리!");
+                    Console.WriteLine($"남은 체력 : {player.getHp()}");
+                    break;
+                }
+
+                damage = monster.getAttack();
+                player.onDamaged(damage);
+                if (player.isDead())
+                {
+                    Console.WriteLine("패배!");
+                    Console.WriteLine($"남은 체력 : {player.getHp()}");
+                    mode = GameMode.Lobby;
+                    break;  
+                } 
+            }     
+        }
+
+        private void tryEscape()
+        {
+            int randValue = rand.Next(0,101);
+            if(randValue < 33)
+                mode = GameMode.Town;
+            else
+                processFight();            
+        }
+
     }
 }
